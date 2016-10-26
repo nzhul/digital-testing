a6195f73898aa55213dbf07cb30051efeccbfc4b
GoranGit <gorancvetkov_sr@yahoo.com>
2016-10-26 18:57:14 +0300

    cron modal

---
+++ src/js/cron.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var myWindow = $("#window"),
+        undo = $("#undo");
+
+    undo.click(function () {
+        myWindow.data("kendoWindow").open();
+        undo.fadeOut();
+    });
+
+    function onClose() {
+        undo.fadeIn();
+    }
+
+    myWindow.kendoWindow({
+        width: "600px",
+        title: "Scheduling tests run",
+        visible: false,
+        actions: [
+            "Close"
+        ],
+        close: onClose
+    }).data("kendoWindow").center().open();
+
+    $("#period").kendoComboBox();
+    $("#time").kendoComboBox();
+
+});
