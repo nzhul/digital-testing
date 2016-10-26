62d9bb30f023156bed44760a7eb080739fe37f0f
yavorski <yaforski@gmail.com>
2016-10-26 12:06:16 +0300

    font-awesome

--- src/js/_index.js
+++ src/js/_index.js
@@ -2,5 +2,6 @@
 import asynccss from './async-css'
 
 asynccss.load('https://fonts.googleapis.com/css?family=Roboto:400,500,700')
+asynccss.load('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')
 asynccss.load('https://da7xgjtj801h2.cloudfront.net/2016.1.226/styles/kendo.common.min.css')
 asynccss.load('https://da7xgjtj801h2.cloudfront.net/2016.1.226/styles/kendo.material.min.css')
